081df52d23c93f28fd1c919a437da38b240aa46b
Rachele Edwards <dot.com1472@gmail.com>
2020-03-24 13:45:35 -0600

    was able to fix the issue by passing in movie in Movie.js card--all app is funcitonal again

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -29,29 +29,4 @@ const MovieList = props => {
   );
 }
 
-// function MovieDetails({ movie }) {
-//   const { title, director, metascore, stars } = movie;
-//   return (
-//     <div cla>
-//       <div className="movie-card">
-//         <h2><Link to={`/movies/${movie.id}`}>{title}</Link></h2>
-//         <div className="movie-director">
-//           Director: <em>{director}</em>
-//         </div>
-//         <div className="movie-metascore">
-//           Metascore: <strong>{metascore}</strong>
-//         </div>
-//         <h3>Actors</h3>
-
-//         {stars.map(star => (
-//           <div key={star} className="movie-star">
-//             {star}
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-
-// }
-
 export default MovieList;
